93456657ae1633fdca7f5792f1fb1c49774fd359
Diogo <diogocorrea@tecnomotor.com.br>
2022-07-15 17:26:18 -0300

    Implementando listeners nos fragments
    - Cria membros no fragment para representar os listeners: função vazia que não faz e não retorna nada e que pode ser sobrescrita 
    - no onAttachFragment do ListaNoticiasActivity verifica se o fragment é ListaNoticiasFragment e então dentro das suas funções chama as funções da activity
    - desta maneira este fragment pode ser reutilizado por outras activities. A activity geralmente não é reutilizada então não tem problema ter o acoplamento dentro do onAttachFragment da activity

--- app/src/main/java/br/com/alura/technews/ui/fragment/ListaNoticiasFragment.kt
+++ app/src/main/java/br/com/alura/technews/ui/fragment/ListaNoticiasFragment.kt
@@ -9,6 +9,7 @@ import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import br.com.alura.technews.R
+import br.com.alura.technews.model.Noticia
 import br.com.alura.technews.ui.fragment.extensions.mostraErro
 import br.com.alura.technews.ui.recyclerview.adapter.ListaNoticiasAdapter
 import br.com.alura.technews.ui.viewmodel.ListaNoticiasViewModel
@@ -27,11 +28,12 @@ class ListaNoticiasFragment : Fragment() {
 
     }
     private val viewModel: ListaNoticiasViewModel by viewModel()
+    var quandoFabSalvaNoticiaClicado: () -> Unit = {} // função vazia que não faz nada e pode ser sobreescrita
+    var quandoNoticiaSeleciona: (noticia: Noticia) -> Unit = {}
 
     // não cria a view, apenas para inicializações
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
@@ -50,7 +52,7 @@ class ListaNoticiasFragment : Fragment() {
 
     private fun configuraFabAdicionaNoticia() {
         lista_noticias_fab_salva_noticia.setOnClickListener {
-//            abreFormularioModoCriacao()
+            quandoFabSalvaNoticiaClicado()
         }
     }
 
@@ -62,7 +64,7 @@ class ListaNoticiasFragment : Fragment() {
     }
 
     private fun configuraAdapter() {
-//        adapter.quandoItemClicado = this::abreVisualizadorNoticia
+        adapter.quandoItemClicado = quandoNoticiaSeleciona
     }
 
     private fun buscaNoticias() {
