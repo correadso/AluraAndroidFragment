7d950defcc1db4427597c34292ada63fd0944b71
Diogo <diogocorrea@tecnomotor.com.br>
2022-07-20 00:58:05 -0300

    Reutilizando fragments Activity
    - Renomeando a ListaNoticiasActivity para NoticiasActivity, pois uma única Activity conterá dois ou mais Fragments: a lista de notícias e a exibiçã ode uma notícia
    - a NoticiasActivity vai chamar um fragment no oncreate utilizando o gerenciador de fragments e na função abreVisualizadorNoticia vai chamar o outro fragment enviando o id da notícia.
    - a criação do fragment em VisualizaNoticiaActivity foi removido e colocado em NoticiasActivity passando a notícia como parâmetro para poder pegar o id e enviar para o fragment posteriormente buscar pelo id da notícia para exibir na tela

--- app/src/main/java/br/com/alura/technews/ui/fragment/VisualizaNoticiaFragment.kt
+++ app/src/main/java/br/com/alura/technews/ui/fragment/VisualizaNoticiaFragment.kt
@@ -24,7 +24,7 @@ class VisualizaNoticiaFragment : Fragment() {
         arguments?.getLong(NOTICIA_ID_CHAVE) ?: throw IllegalArgumentException("Id inválido")
     }
     private val viewModel: VisualizaNoticiaViewModel by viewModel { parametersOf(noticiaId) }
-    var quandoSelecionaMenuEdicao: () -> Unit = {}
+    var quandoSelecionaMenuEdicao: (noticia: Noticia) -> Unit = {}
     var quandoFinalizaTela: () -> Unit = {}
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,7 +49,9 @@ class VisualizaNoticiaFragment : Fragment() {
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         when (item?.itemId) {
-            R.id.visualiza_noticia_menu_edita -> quandoSelecionaMenuEdicao()
+            R.id.visualiza_noticia_menu_edita -> {
+                viewModel.noticiaEncontrada.value?.let(quandoSelecionaMenuEdicao)
+            }
             R.id.visualiza_noticia_menu_remove -> remove()
         }
         return super.onOptionsItemSelected(item)
